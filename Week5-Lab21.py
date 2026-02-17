1aa146a7b87373daec4ce226a452a1476ed107d0
HGSIE12 <xhmode_sa@hotmail.com>
2026-02-17 18:40:46 +0300

    Load and process housing data with pandas
    Added data loading and processing for housing data.

--- Week5-Lab21.py
+++ Week5-Lab21.py
@@ -1,1 +1,18 @@
+import pandas as pd
+
+#1) Load housing data
+
+df = pd.read_csv("day21_housing.csv")
+print(df)
+
+df["price_per_sqft"] = df["price"] / df["sqft"]
+print(df)
+
+df["room"] = df["bathrooms"] + df["bedrooms"]
+print(df)
+
+df["high_sqft"] = df["sqft"].apply(
+    lambda x: "high" if x > 1700 else "low"
+)
+print(df)
 
